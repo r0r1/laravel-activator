6566828b4c3d6039b8b06ab3b513d94d0277cf9c
Rori Kurniadi <rori.kurniadi@yahoo.com>
2016-10-09 00:30:59 +0700

    modify config activator

--- config/activator.php
+++ config/activator.php
@@ -3,4 +3,10 @@
 return [
     // handle laravel notification via mail
     'notification'  => true,
+
+    // User model
+    'model'  => App\User::class,
+
+    // expiryTime
+    'expiry_time'   => 60
 ];
