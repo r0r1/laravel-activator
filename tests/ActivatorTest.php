f39719e872cf3ee963e974be0a835fc52d07dc30
Rori Kurniadi <rori.kurniadi@yahoo.com>
2016-10-09 12:48:15 +0700

    add phpunit

---
+++ tests/ActivatorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Rorikurn\Activator\Activator;
+
+class ActivatorTest extends PHPUnit_Framework_TestCase
+{
+    public function test_scopes_can_be_managed()
+    {
+        Passport::tokensCan([
+            'user' => 'get user information',
+        ]);
+        $this->assertTrue(Passport::hasScope('user'));
+        $this->assertEquals(['user'], Passport::scopeIds());
+        $this->assertEquals('user', Passport::scopes()[0]->id);
+    }
+}
